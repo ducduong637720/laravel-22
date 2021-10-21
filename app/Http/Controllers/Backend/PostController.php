72a2d2b98bb42b4f5616f8fd65a6a143744985bf
ducduong637720 <ducxauzaifa2k@gmail.com>
2021-10-21 21:53:19 +0700

    lam 16.3

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -69,7 +69,7 @@ class PostController extends Controller
             [
                 'title' => 'required|unique:posts|max:255',
                 'content' => 'required',
-         
+                'status' => 'required|in:0,1,2',
             ],
             [
                 'required' => 'Thuộc tính :attribute là bắt buộc',
